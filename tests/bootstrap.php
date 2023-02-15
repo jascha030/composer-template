41375d0e5096e573ccc51054c46c3669d192a591
Jascha030 <contact@jaschavanaalst.nl>
2023-02-16 00:57:12 +0100

    ♻️ Simplify autoloading in tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,5 @@
 
 declare(strict_types=1);
 
-(static function (): void {
-    require_once dirname(__DIR__) . '/vendor/autoload.php';
-})();
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
