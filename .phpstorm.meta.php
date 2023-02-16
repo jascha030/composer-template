e7060b5207508c15694e484e9240e5eec5f3becc
Jascha030 <contact@jaschavanaalst.nl>
2023-02-16 01:02:12 +0100

    🔧 Update quality tools config files

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,10 +2,5 @@
 
 namespace PHPSTORM_META
 {
-    override(\Psr\Container\ContainerInterface::get(0), map([
-        '' => '@',
-    ]));
-    override(\DI\Container::get(0), map([
-        '' => '@',
-    ]));
+    override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
 }
